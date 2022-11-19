88fddd10ba17dcc02494b6a53b5927539bdbfb8c
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-11-19 23:47:02 +0800

    fuck

--- Engine.py
+++ Engine.py
@@ -1,24 +1,6 @@
 from Data import * 
 
 class Engine:
-    def __init__(self):
-        self.costs = dict()  # for storing the cost of each node for the program process
-        self.path = dict()  # for storing the path
-        self.totalcosts = dict()  # for storing the total cost of the path
-        self.line = dict()  # for storing the subway line of the path
-        self.total_walkTime = dict()  # for storing the total walking time of the path
-        self.smallcost = dict()  # for storing the sum of the cost and heuristic of the node
-        for i in nodes:
-            # costs array contain the distance and the heurisitc of current node
-            self.costs[i] = [999999,0]
-            self.path[i] = ' '
-            self.totalcosts[i] = 999999
-            self.line[i] = []
-            self.total_walkTime[i] = 0
-            self.smallcost[i] = 999999
-        self.open = set()
-        self.closed = set()
-
     # for checking if the line is changed
     def __checkChangeLine(self, curLine, curNode, nextNode, fromLine):
         if len(curLine) == 0:
@@ -123,34 +105,53 @@ class Engine:
                 totalcosts, line, total_walkTime, stations, smallcost)
     
     def get(self, start_str, end_str):
-        start_node = list(stations.keys())[list(
-            stations.values()).index(start_str)]
-        self.open.add(start_node)
-        self.path[start_node] = start_str
-        self.costs[start_node] = [999999,0]
-        self.totalcosts[start_node] = 0
-        self.total_walkTime[start_node] = 0
-        self.smallcost[start_node] = 0
+        costs = dict()  # for storing the cost of each node for the program process
+        self.path = dict()  # for storing the path
+        totalcosts = dict()  # for storing the total cost of the path
+        line = dict()  # for storing the subway line of the path
+        total_walkTime = dict()  # for storing the total walking time of the path
+        smallcost = dict()  # for storing the sum of the cost and heuristic of the node
+        for i in nodes:
+            # costs array contain the distance and the heurisitc of current node
+            costs[i] = [999999, 0]
+            self.path[i] = ' '
+            totalcosts[i] = 999999
+            line[i] = []
+            total_walkTime[i] = 0
+            smallcost[i] = 999999
+        open = set()
+        closed = set()
 
-        if end_str in airportLine1 and end_str in lines["Airport Express"]:
+        # initial the data
+        start_node_str = start_str
+        start_node = list(stations.keys())[list(
+            stations.values()).index(start_node_str)]
+        open.add(start_node)
+        self.path[start_node] = start_node_str
+        costs[start_node] = [0, 0]
+        totalcosts[start_node] = 0
+        total_walkTime[start_node] = 0
+        smallcost[start_node] = 0
+
+        goal_node_str = end_str
+        if start_node_str in airportLine1 and goal_node_str in lines["Airport Express"]:
             goal_node = list(airportLine2Value.keys())[list(
-            airportLine2Value.values()).index(end_str)]
-        elif end_str in airportLine2 and end_str in airportLine1:
+                airportLine2Value.values()).index(goal_node_str)]
+        elif start_node_str in airportLine2 and goal_node_str in airportLine1:
             goal_node = list(airportLine1Value.keys())[list(
-            airportLine1Value.values()).index(end_str)]
+                airportLine1Value.values()).index(goal_node_str)]
         else:
             goal_node = list(stations.keys())[list(
-            stations.values()).index(end_str)]
+                stations.values()).index(goal_node_str)]
 
         h = heuristics[goal_node]
         # program start
-        self.__A_star(graph, self.costs, self.open, self.closed, start_node,
-            h,self.totalcosts, self.line, self.total_walkTime, stations, self.smallcost)
-
+        self.__A_star(graph, costs, open, closed, start_node,
+            h, totalcosts, line, total_walkTime, stations, smallcost)
         return {
             "route": self.path[goal_node],
-            "interchange": len(self.line[goal_node])-1,
-            "line": self.line[goal_node],
-            "cost": self.totalcosts[goal_node],
-            "walking": self.total_walkTime[goal_node]
+            "interchange": len(line[goal_node])-1,
+            "line": line[goal_node],
+            "cost": totalcosts[goal_node],
+            "walking": total_walkTime[goal_node]
         }
